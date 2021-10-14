62a365a2dbd7e62a67d77621755ecc4b84d691c0
saratbhaskaran <sarathkaruvanthala@gmail.com>
2021-10-14 17:48:42 +0530

    sabiqgraamnochanges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input,Col,Button } from 'reactstrap';
+class Sabiqgram extends React.Component {
+  state={
+    paragraph:"",
+    id:0,
+    likes:"",
+    store:[],
+    
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  }
+
+   storeDuplicate=[]
+
+   post = () => {
+
+     this.state.id += 1;
+     let obj={}
+     obj={id:this.state.id,paragraph:this.state.paragraph}
+    //  this.state.obj=obj;
+    // console.log("this.state.obj",this.state.obj)
+
+     
+     
+     this.storeDuplicate.push(obj)
+     console.log("this.state.storeDuplicate",this.storeDuplicate)
+    //  this.setState({store:storeDuplicate})
+     this.setState({store:this.storeDuplicate})
+     
+    //  console.log(this.state.store)
+    // [ …this.state.store, …this.storeDulipicare ]
+    
+     
+   }
+  
+  render() { 
+    
+    return (
+      <div>
+      
+      <h1>Sabiqgram</h1>
+      <Col md="4">
+
+       <Input type="textarea" value={this.state.paragraph} onChange={(text)=>{this.setState({paragraph: text.target.value} )}}></Input>
+    
+       <Button color="primary" onClick={()=> this.post() }>POST</Button>
+      </Col>
+          
+          {this.state.store.map(para=>{
+            return <p>{para}</p>
+            
+          })}
+
     </div>
-  );
+    );
+  }
 }
-
-export default App;
+ 
+export default Sabiqgram;
